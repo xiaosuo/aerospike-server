8f1d15211c2035e5d4484a03e9234d630f48930e
Andrew Gooding <andy@aerospike.com>
2015-01-27 14:11:44 -0800

    AER-3191 - Added proto error code AS_PROTO_RESULT_FAIL_FORBIDDEN (22). Also added new write error stats for this and other error codes previously without their own stats: err_write_fail_forbidden, err_write_fail_record_too_big, err_write_fail_bin_name, err_write_fail_bin_not_found.

--- as/include/base/proto.h
+++ as/include/base/proto.h
@@ -70,6 +70,7 @@ struct as_file_handle_s;
 #define AS_PROTO_RESULT_FAIL_KEY_MISMATCH 19
 #define AS_PROTO_RESULT_FAIL_NAMESPACE 20
 #define AS_PROTO_RESULT_FAIL_BIN_NAME 21
+#define AS_PROTO_RESULT_FAIL_FORBIDDEN 22 // operation (perhaps temporarily) not possible
 
 // Security result codes. Must be <= 255, to fit in one byte. Defined here to
 // ensure no overlap with other result codes.
