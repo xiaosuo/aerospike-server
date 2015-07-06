89e7fa1e5d8a02f25cc09255b0f0bc3e5d2b342a
Brian Nichols <bnichols@aerospike.com>
2015-07-06 10:28:47 -0700

    Add float features string.

--- as/src/base/thr_info.c
+++ as/src/base/thr_info.c
@@ -7129,7 +7129,7 @@ as_info_init()
 	as_info_set("node", istr, true);                     // Node ID. Unique 15 character hex string for each node based on the mac address and port.
 	as_info_set("name", istr, false);                    // Alias to 'node'.
 	// Returns list of features supported by this server
-	as_info_set("features", "batch-index;replicas-all;replicas-master;replicas-prole;udf", true);
+	as_info_set("features", "float;batch-index;replicas-all;replicas-master;replicas-prole;udf", true);
 	if (g_config.hb_mode == AS_HB_MODE_MCAST) {
 		sprintf(istr, "%s:%d", g_config.hb_addr, g_config.hb_port);
 		as_info_set("mcast", istr, false);               // Returns the multicast heartbeat address and port used by this server. Only available in multicast heartbeat mode.
